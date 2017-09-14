c994774693d1005b8bfb210ccff9386f8f552118
bbaxa16 <brooke769@gmail.com>
2017-09-14 16:06:55 -0600

    successfull request from api

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log('howdy');
+const app = angular.module('voveo', []);
+
+app.controller('mainController', ['$http', function($http){
+
+  this.getPolling = function(){
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/civicinfo/v2/elections' + '?key=AIzaSyBeJbFpC9D_pouOsmIW_9NWnwp56HmcORw' 
+    }).then(function(response){
+      console.log('success');
+      console.log(response);
+    }, function(err){
+      console.log('fail');
+      console.log(err);
+    })
+  }
+
+  this.getPolling()
+}]);
