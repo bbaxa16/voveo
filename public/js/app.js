daf75e8dbf6f08f5bd7a79659ec43f4fa447dc20
bbaxa16 <brooke769@gmail.com>
2017-09-18 08:12:01 -0600

    clean up

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,23 @@ const app = angular.module('voveo', []);
 
 app.controller('mainController', ['$http', function($http){
 
-  this.getPolling = function(){
+  this.getPolling = function(address, city, state){
     $http({
       method: 'GET',
-      url: 'https://www.googleapis.com/civicinfo/v2/elections' + '?key=AIzaSyBeJbFpC9D_pouOsmIW_9NWnwp56HmcORw' 
+      url: 'https://www.googleapis.com/civicinfo/v2/voterinfo' + '?key==' + address.split('').join('%20') + '%20' + city.split('').join('%20') + '%20' + state +'%20KS&electionId=2000'
     }).then(function(response){
       console.log('success');
-      console.log(response);
+      console.log(response.data.pollingLocations[0]);
     }, function(err){
       console.log('fail');
       console.log(err);
     })
   }
+ const str = 'this is a string'
 
-  this.getPolling()
+ this.join = function(str){
+   console.log( str.split(' ').join('%20'));
+ }
+this.join(str)
+//  this.getPolling()
 }]);
