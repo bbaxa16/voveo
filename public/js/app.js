22eafe3ac9bb0389154b7db944e741afe913655c
bbaxa16 <brooke769@gmail.com>
2017-09-19 09:43:12 -0500

    put the address form into a separate html file

--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,27 @@ const app = angular.module('voveo', []);
 
 app.controller('mainController', ['$http', function($http){
 
-  this.getPolling = function(address, city, state){
+  this.getPolling = function(address){
     $http({
       method: 'GET',
-      url: 'https://www.googleapis.com/civicinfo/v2/voterinfo' + '?key==' + address.split('').join('%20') + '%20' + city.split('').join('%20') + '%20' + state +'%20KS&electionId=2000'
+      url: 'https://www.googleapis.com/civicinfo/v2/representatives' + '?key=AIzaSyBeJbFpC9D_pouOsmIW_9NWnwp56HmcORw&address=' + this.address.split(' ').join('%20') + '%20' + this.city.split(' ').join('%20') + '%20' + this.state.split(' ').join('%20')
     }).then(function(response){
       console.log('success');
-      console.log(response.data.pollingLocations[0]);
+      console.log(response.data);
     }, function(err){
       console.log('fail');
       console.log(err);
     })
+  }
+  this.test = function(address){
+    console.log(address.split(' ').join('%20'));
+
   }
  const str = 'this is a string'
 
  this.join = function(str){
    console.log( str.split(' ').join('%20'));
  }
-this.join(str)
-//  this.getPolling()
+//this.join(str)
+//this.getPolling()
 }]);
