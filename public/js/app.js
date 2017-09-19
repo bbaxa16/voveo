3d7ffe8606e8a61496180730aeb258d19a11306e
bbaxa16 <brooke769@gmail.com>
2017-09-19 16:14:54 -0500

    create successful login route

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,29 @@
 const app = angular.module('voveo', []);
 
 app.controller('mainController', ['$http', function($http){
-
-  this.getPolling = function(address){
+  const controller = this;
+  this.url = 'http://localhost:3000/';
+  this.getReps = function(address){
     $http({
       method: 'GET',
       url: 'https://www.googleapis.com/civicinfo/v2/representatives' + '?key=AIzaSyBeJbFpC9D_pouOsmIW_9NWnwp56HmcORw&address=' + this.address.split(' ').join('%20') + '%20' + this.city.split(' ').join('%20') + '%20' + this.state.split(' ').join('%20')
     }).then(function(response){
       console.log('success');
-      console.log(response.data);
+      controller.officials = response.data.officials
+      console.log(controller.officials);
     }, function(err){
       console.log('fail');
       console.log(err);
     })
   }
-  this.test = function(address){
-    console.log(address.split(' ').join('%20'));
-
+  this.login = function(userPass){
+    $http({
+      method: 'POST',
+      url: this.url + '/users/login',
+      data: { user: { username: userPass.username, password: userPass.password }},
+    }).then(function(response){
+      console.log(response);
+    })
   }
- const str = 'this is a string'
 
- this.join = function(str){
-   console.log( str.split(' ').join('%20'));
- }
-//this.join(str)
-//this.getPolling()
 }]);
