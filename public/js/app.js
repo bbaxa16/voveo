b6a4fe8beb18fdd66f3afbb7a13225183299941a
bbaxa16 <brooke769@gmail.com>
2017-09-22 20:27:12 -0500

    update

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ app.controller('mainController', ['$http', function($http){
   this.getReps = function(address){
     $http({
       method: 'GET',
-      url: 'https://www.googleapis.com/civicinfo/v2/representatives' + '?key=AIzaSyBeJbFpC9D_pouOsmIW_9NWnwp56HmcORw&address=' + this.address.split(' ').join('%20') + '%20' + this.city.split(' ').join('%20') + '%20' + this.state.split(' ').join('%20')
+      url: 'https://www.googleapis.com/civicinfo/v2/representatives' + '?key=&address=' + this.address.split(' ').join('%20') + '%20' + this.city.split(' ').join('%20') + '%20' + this.state.split(' ').join('%20')
     }).then(function(response){
       console.log('success');
       controller.officials = response.data.officials
