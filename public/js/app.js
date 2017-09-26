d67af5f8e5ce9cbaf7187b9aac576a50b7bc796e
bbaxa16 <brooke769@gmail.com>
2017-09-26 16:51:21 -0500

    cleaned up js, created a new view

--- public/js/app.js
+++ public/js/app.js
@@ -7,15 +7,11 @@ app.controller('mainController', ['$http', function($http){
   const controller = this;
 
   this.url = 'http://localhost:3000/';
-  this.loginForm = false;
   this.registerForm = false;
+  this.repz = false;
   this.toggleRegister = function() {
     this.registerForm = !this.registerForm;
     this.loginForm = false;
-  }
-  this.toggleLogin = function() {
-    this.loginForm = !this.loginForm;
-    this.registerForm = false;
   }
   this.toggleHelp = function() {
     this.helpModal = !this.helpModal;
@@ -31,7 +27,9 @@ app.controller('mainController', ['$http', function($http){
       controller.city = response.data.normalizedInput.city;
       controller.state = response.data.normalizedInput.state;
       controller.zip = response.data.normalizedInput.zip;
-      controller.newdata(1);
+      //uncomment this function when you're ready to save data again.
+      //controller.newdata(1);
+      controller.repz = true;
     }, function(err){
       console.log('fail');
       console.log(err);
@@ -49,43 +47,9 @@ app.controller('mainController', ['$http', function($http){
       console.log(response);
       this.loginForm = false;
       this.registerForm = false;
-      //controller.checkLogin();
     }.bind(this))
   }
-  this.login = function(userPass){
-    $http({
-      method: 'POST',
-      url: this.url + '/users/login',
-      data: { user: { username: userPass.username, password: userPass.password }},
-    }).then(function(response){
-      if(response.data.message === 'Unauthorized'){
-        console.log(response);
-        this.error = 'username or password was incorrect'
-        this.unauthorized = true;
-      }
-      else {
-        this.user = response.data.user;
-        localStorage.setItem('token', JSON.stringify(response.data.token));
-        localStorage.setItem('logged', JSON.stringify(true));
-        this.checkLogin();
-        this.loginForm = false;
-        this.registerForm = false;
-        this.unauthorized = false;
-      }
-    }.bind(this));
-  }
-  this.logout = function() {
-    localStorage.clear('token');
-    location.reload();
-  }
-  this.checkLogin = function() {
-    if (localStorage.logged === "true"){
-      controller.logged = true;
-    }
-    else {
-      controller.logged = false;
-    }
-  }
+
   this.getUsers = function(){
     $http({
       url: this.url + '/users',
@@ -102,23 +66,18 @@ app.controller('mainController', ['$http', function($http){
       }
     }.bind(this));
   }
-  this.printshit = function() {
-    console.log('OH SHIII');
-    console.log(this.ish);
-  }
-  this.checkLogin();
   this.newdata = function(user_id) {
-  $http({
-    url: this.url + '/users/' + user_id + '/data',
-    method: 'POST',
-    data: {
-      city: this.city,
-      state: this.state,
-      zip: this.zip }
-  }).then(function(response){
-    console.log('this is creating a new data thing');
-    console.log(response);
-  })
+$http({
+  url: this.url + '/users/' + user_id + '/data',
+  method: 'POST',
+  data: {
+    city: this.city,
+    state: this.state,
+    zip: this.zip }
+}).then(function(response){
+  console.log('this is creating a new data thing');
+  console.log(response);
+})
 }
 
   // this.memberinfo = function(){
