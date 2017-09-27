a3c1f96ce35683b7411196e005d51c4dd6311b9c
bbaxa16 <brooke769@gmail.com>
2017-09-26 19:34:09 -0500

    added and connected a file for jquery

--- public/js/app.js
+++ public/js/app.js
@@ -22,6 +22,7 @@ app.controller('mainController', ['$http', function($http){
       url: 'https://www.googleapis.com/civicinfo/v2/representatives' + '?key=AIzaSyBeJbFpC9D_pouOsmIW_9NWnwp56HmcORw&address=' + this.address.split(' ').join('%20') + '%20' + this.city.split(' ').join('%20') + '%20' + this.state.split(' ').join('%20')
     }).then(function(response){
       console.log('success');
+      console.log(response.data);
       controller.officials = response.data.officials
       controller.offices = response.data.offices
       controller.city = response.data.normalizedInput.city;
